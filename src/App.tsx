1ec696ef8cfe5cd90860fcb4c0b3dcfcac6b6d5a
mudit sarda <muditsarda23@gmail.com>
2024-04-14 11:20:11 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export type Crypto = {
+  ath: number;
+  atl: number;
+  current_price: number;
+  high_24h: number;
+  id: string;
+  low_24h: number;
+  market_cap_rank: number;
+  max_supply: number;
+  name: string;
+  symbol: string;
+};
 
 function App() {
+  // we want cryptos to be an arrAY OF Crypto or null
+  const [cryptos, setCryptos] = useState<Crypto[] | null>();
+
+  useEffect(() => {
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+    axios.get(url).then((response) => {
+      setCryptos(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cryptos
+        ? cryptos.map((crypto) => {
+            return <p>{crypto.name}</p>;
+          })
+        : null}
     </div>
   );
 }
